a746d0096108551e3abe08b1b26bc3a3d3e28505
ilias ed-darhmoumi <ilias.ed-darhmoumi@inventiv-it.fr>
2026-06-03 21:31:43 +0100

    implement other thumbnail routes

--- controllers/ThumbnailController.ts
+++ controllers/ThumbnailController.ts
@@ -1,6 +1,80 @@
 import { Request, Response } from "express"
+import mongoose from "mongoose"
 import Thumbnail from "../models/Thumbnail.js"
 import generateImage from "../configs/ai.js"
+
+export const getThumbnails = async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.session
+    const thumbnails = await Thumbnail.find({ userId }).sort({ createdAt: -1 })
+
+    res.json({
+      total: thumbnails.length,
+      thumbnails,
+    })
+  } catch (error) {
+    console.log(error)
+
+    res.status(500).json({ error })
+  }
+}
+
+export const getThumbnailById = async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.session
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        message: "Invalid thumbnail id",
+      })
+    }
+
+    const thumbnail = await Thumbnail.findOne({ userId, _id: id })
+
+    if (!thumbnail) {
+      return res.status(404).json({
+        message: "Thumbnail not found",
+      })
+    }
+
+    res.json({ thumbnail })
+  } catch (error) {
+    console.log(error)
+
+    res.status(500).json({ error })
+  }
+}
+
+export const deleteThumbnail = async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.session
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        message: "Invalid thumbnail id",
+      })
+    }
+
+    const thumbnail = await Thumbnail.findOneAndDelete({
+      _id: id,
+      userId,
+    })
+
+    if (!thumbnail) {
+      return res.status(404).json({
+        message: "Thumbnail not found",
+      })
+    }
+
+    res.json({ message: "Thumbnail deleted successfully" })
+  } catch (error) {
+    console.log(error)
+
+    res.status(500).json({ error })
+  }
+}
 
 export const generateThumbnail = async (req: Request, res: Response) => {
   try {
--- routes/ThumbnailRoutes.ts
+++ routes/ThumbnailRoutes.ts
@@ -1,9 +1,17 @@
 import express from "express"
-import { generateThumbnail } from "../controllers/ThumbnailController.js"
+import {
+  deleteThumbnail,
+  generateThumbnail,
+  getThumbnailById,
+  getThumbnails,
+} from "../controllers/ThumbnailController.js"
 import protect from "../middleware/auth.js"
 
 const ThumbnailRouter = express.Router()
 
+ThumbnailRouter.get("/", protect, getThumbnails)
+ThumbnailRouter.get("/:id", protect, getThumbnailById)
 ThumbnailRouter.post("/generate", protect, generateThumbnail)
+ThumbnailRouter.delete("/:id", protect, deleteThumbnail)
 
 export default ThumbnailRouter
